4b38b76ebc93a76c4c3cdf59fb461af6cba03439
silva <ericoderito@gmail.com>
2022-07-16 17:39:03 +0300

    add html content
    customize tailwindcss theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-red": "hsl(0, 78%, 62%)",
+        "primary-cyan": "hsl(180, 62%, 55%)",
+        "primary-orange": "hsl(34, 97%, 64%)",
+        "primary-blue": "hsl(212, 86%, 64%)",
+        "dark-blue": "hsl(234, 12%, 34%)",
+        "grayish-blue": "hsl(229, 6%, 66%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+      },
+      fontFamily: {
+        "body": ["Poppins", 'sans-serif']
+      }
+    },
   },
   plugins: [],
-}
+};
